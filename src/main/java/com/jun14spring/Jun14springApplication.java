62512187124496cd0c7260d0e15032e128c9e30e
Matt Cadorette <mcadorette@gmail.com>
2019-06-14 10:38:23 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/jun14spring/Jun14springApplication.java
@@ -0,0 +1,12 @@
+package com.jun14spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jun14springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jun14springApplication.class, args);
+	}
+}
